06aaa794a140e94adb913847d88f54b98b601381
AsylbekA <@anarbayasylbek@gmail.com>
2020-06-04 00:24:28 +0600

    Потоки с параметрами и ParameterizedThreadStart

--- Thread/Program.cs
+++ Thread/Program.cs
@@ -7,26 +7,67 @@ namespace Threaddddd
     {
         static void Main(string[] args)
         {
-            Thread thread = new Thread(new ThreadStart(Count));
-            thread.Start();
-            
-            for (int i = 1; i < 10; i++)
-            {
-                Console.WriteLine("Base potok");
-                Console.WriteLine(i*i);
-                Thread.Sleep(900);
-            }
-            Console.Read();
-        }
+            //    Thread thread = new Thread(new ThreadStart(Count));
+            //    thread.Start();
 
-        private static void Count()
+            //    for (int i = 1; i < 10; i++)
+            //    {
+            //        Console.WriteLine("Base potok");
+            //        Console.WriteLine(i*i);
+            //        Thread.Sleep(900);
+            //    }
+            //    Console.Read();
+            //}
+
+            //private static void Count()
+            //{
+            //    for(int i = 1; i < 20; i++)
+            //    {
+            //        Console.WriteLine("Second potok");
+            //        Console.WriteLine(i * i);
+            //        Thread.Sleep(5000);
+            //    }
+
+            //Class cl = new Class();
+            //cl.x = 5;
+            //cl.y = 7;
+            //int number = 5;
+
+            Class c = new Class(6, 7);
+
+            Thread thread = new Thread(new ThreadStart(c.Count));
+            thread.Start();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Base ptotk");
+            //    Console.WriteLine(i * i);
+            //    Thread.Sleep(500);
+            //}
+        }
+    }
+
+    public class Class
+    {
+        public int x;
+        public int y;
+
+        public Class(int x, int y)
         {
-            for(int i = 1; i < 20; i++)
+            this.x = x;
+            this.y = y;
+        }
+        public  void Count()
+        {
+            for (int i = 0; i < 15; i++)
             {
+                //Class c = (Class)obj;
                 Console.WriteLine("Second potok");
-                Console.WriteLine(i * i);
-                Thread.Sleep(5000);
+                Console.WriteLine(i * i * x * y);
+                Thread.Sleep(500);
             }
         }
     }
+
+
 }
