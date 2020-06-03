a1d63a9278d057b847ecc56c225d105f5b50461e
AsylbekA <@anarbayasylbek@gmail.com>
2020-06-03 23:50:57 +0600

    отдельных потоках методы без параметров

--- Thread/Program.cs
+++ Thread/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading;
 
-namespace Thread
+namespace Threaddddd
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread thread = new Thread(new ThreadStart(Count));
+            thread.Start();
+            
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Base potok");
+                Console.WriteLine(i*i);
+                Thread.Sleep(900);
+            }
+            Console.Read();
+        }
+
+        private static void Count()
+        {
+            for(int i = 1; i < 20; i++)
+            {
+                Console.WriteLine("Second potok");
+                Console.WriteLine(i * i);
+                Thread.Sleep(5000);
+            }
         }
     }
 }
